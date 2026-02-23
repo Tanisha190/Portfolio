db7a53397910ddd628d3ab40d802f0105fdbdecc
Tanisha G <gtanisha705@gmail.com>
2026-02-23 16:06:38 +0100

    case sensitivity issue

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,12 +1,13 @@
 import React from "react";
-import { projects } from "../data/Projects";
+import { projects } from "../data/projectsdata";
+import { type Project } from "../types/Project";
 
 const Projects: React.FC = () => {
   return (
     <section id="projects" className="section">
       <h2>Projects</h2>
       <div className="projects-container">
-        {projects.map((project, index) => (
+        {projects.map((project: Project, index: number) => (
           <div key={index} className="project-card">
             <h3>{project.title}</h3>
             {project.image && (
