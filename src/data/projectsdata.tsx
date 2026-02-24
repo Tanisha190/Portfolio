b6ae7a020b8b47d6cf14a8a45b2e06dbd1d517fe
Tanisha G <gtanisha705@gmail.com>
2026-02-24 16:52:59 +0100

    added my second project

--- src/data/projectsdata.tsx
+++ src/data/projectsdata.tsx
@@ -1,10 +1,17 @@
 import { type Project} from "../types/Project";
 import portfolio from "../../public/assets/portfolio.png";
+import nest from "../../public/assets/nest.png";
 
 export const projects: Project[] = [
   {
     title: "Portfolio",
     github: "https://github.com/Tanisha190/Portfolio",
     image: portfolio,
-  }
+  },
+  {
+    title: "Nest.js Boilerplate",
+    github: "https://github.com/Tanisha190/Nest.js-Boilerplate",
+    image: nest,
+  },
+  
 ];
